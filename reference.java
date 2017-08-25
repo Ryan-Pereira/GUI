a3513aac484a7736d6955ad3a8b44f388f721092
Ryan-Pereira <31357562+Ryan-Pereira@users.noreply.github.com>
2017-08-25 18:31:02 -0400

    Delete reference.java

--- reference.java
+++
@@ -1,7 +0,0 @@
-package com.Pokeeee;
-
-public class reference {
- public static final String Mod_ID = "AJS";
- public static final String Mod_NAME = "AJS Guild Mod";
- public static final String VERSION = "1.0";
-}
